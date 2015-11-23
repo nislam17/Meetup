4ef43659f59f85aca0856da55d56eb67aca07a1f
Matthew Laikhram <mlaikhram@hotmail.com>
2015-11-22 21:55:01 -0500

    please work

--- dbproject/include.php
+++ dbproject/include.php
@@ -3,7 +3,7 @@
 
 //<!-- Example Blog written by Raymond Mui -->
 
-$mysqli = new mysqli("localhost", "root", "", "blog");
+$mysqli = new mysqli("localhost", "root", "", "dbproject");
 
 /* check connection */
 if (mysqli_connect_errno()) {
